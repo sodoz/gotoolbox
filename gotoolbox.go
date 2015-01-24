2081e213857851ed783ba045b31a977c068f814c
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2015-01-24 11:15:17 +0100

    Wired default index action

--- gotoolbox.go
+++ gotoolbox.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-  "fmt"
+  "github.com/codegangsta/controller"
   "github.com/codegangsta/negroni"
   "github.com/gorilla/mux"
-  "net/http"
+  "gotoolbox/controllers"
 )
 
 func main() {
   r := mux.NewRouter()
-  r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "Welcome to the home page!")
-  })
+  r.Handle("/", controller.Action((*controllers.ApplicationController).Index))
 
   n := negroni.Classic()
   n.UseHandler(r)
