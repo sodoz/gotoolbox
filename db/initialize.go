531999b934ba39191aa332b4f85973e8cec3e0f8
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2015-01-25 17:15:53 +0100

    Added category model including some seeds

--- db/initialize.go
+++ db/initialize.go
@@ -16,8 +16,20 @@ func main() {
 
   db.DropTableIfExists(&models.User{})
   db.DropTableIfExists(&models.Project{})
+  db.DropTableIfExists(&models.Category{})
 
   // create database
   db.AutoMigrate(&models.User{})
   db.AutoMigrate(&models.Project{})
+  db.AutoMigrate(&models.Category{})
+
+  // some nice seeding for the categories
+  router := models.Category{Name: "Router", Description: "net/http router"}
+  db.Save(&router)
+
+  webframeworks := models.Category{Name: "Web Frameworks", Description: "Webframeworks available"}
+  db.Save(&webframeworks)
+
+  json := models.Category{Name: "JSON", Description: "JSON Parsing"}
+  db.Save(&json)
 }
