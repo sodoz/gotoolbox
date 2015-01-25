7815506d032fe1f71917748f8b9824d5a3e60e1d
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2015-01-25 23:48:15 +0100

    Updated GA Tracking

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,5 +8,5 @@ jQuery(function($) {
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-13063152-3', 'auto');
+ga('create', 'UA-58946648-1', 'auto');
 ga('send', 'pageview');
