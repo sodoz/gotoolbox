686767921daf64391c91f1c7b1b737739f7ec6e0
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2015-01-25 13:35:50 +0100

    Implemented session store

--- controllers/application_controller.go
+++ controllers/application_controller.go
@@ -2,15 +2,26 @@ package controllers
 
 import (
   "github.com/codegangsta/controller"
+  "github.com/gorilla/sessions"
   "github.com/yosssi/ace"
   "net/http"
 )
 
+var AppSecret = "XXHJHjhjFJKLfdsdfkjsdfkljljlfsjkKLFDJFHUFHg77GFgg77g72gghjfa8z94qbk"
+var sessionStore sessions.Store = sessions.NewCookieStore([]byte(AppSecret))
+
 type ApplicationController struct {
   controller.Base
+  Session *sessions.Session
 }
 
 func (controller *ApplicationController) Init(rw http.ResponseWriter, r *http.Request) error {
+  var err error
+  controller.Session, err = sessionStore.Get(r, "gotoolbox")
+  if err != nil {
+    controller.Base.Init(rw, r)
+    return err
+  }
   return controller.Base.Init(rw, r)
 }
 
@@ -25,7 +36,18 @@ func (controller *ApplicationController) Index() error {
   return nil
 }
 
+func (controller *ApplicationController) saveSession() error {
+  if err := controller.Session.Save(controller.Request, controller.ResponseWriter); err != nil {
+    return err
+  } else {
+    return nil
+  }
+}
+
 func (controller *ApplicationController) Render(template string, scope map[string]interface{}) error {
+  // better put this into action handler
+  controller.saveSession()
+
   tpl, err := ace.Load("views/base", template, &ace.Options{
     DelimLeft:  "<%",
     DelimRight: "%>",
@@ -41,5 +63,8 @@ func (controller *ApplicationController) Render(template string, scope map[strin
 }
 
 func (controller *ApplicationController) Redirect(url string, status int) {
+  // better put this into action handler
+  controller.saveSession()
+
   http.Redirect(controller.ResponseWriter, controller.Request, url, http.StatusFound)
 }
